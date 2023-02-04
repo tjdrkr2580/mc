f930441f5a5c260bfdb593f9f924a0dc11e76f64
tjdrkr2580 <tjdrkr2580@naver.com>
2023-02-05 03:15:26 +0900

    ADD | Intersection Types

--- ts-study/src/App.tsx
+++ ts-study/src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+//Intersection Types 두개의 타입을 합치는 것 같음 <어떻게 보면 조건이지>
+
+type PROFILE = {
+  age: string;
+  city: string;
+};
+
+type LOGIN = {
+  username: string;
+  password: string;
+};
+
+type USER = PROFILE & LOGIN;
+
+const userA: USER = {
+  age: "21",
+  city: "Incheon",
+  username: "dd1d1",
+  password: "dawdaw",
+};
+
+//Union Types
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;

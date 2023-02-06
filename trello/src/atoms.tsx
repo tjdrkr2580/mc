dcee999d95c621019938b23fac996c3532d0561d
tjdrkr2580 <tjdrkr2580@naver.com>
2023-02-07 00:10:04 +0900

    ADD | Recoil todo atom

--- trello/src/atoms.tsx
+++ trello/src/atoms.tsx
@@ -2,5 +2,9 @@ import { atom } from "recoil";
 
 export const todoState = atom({
   key: "todos",
-  default: ["밥먹기", "똥싸기", "잠자기", "공부하기"],
+  default: {
+    to_do: ["밥먹기", "똥싸기", "잠자기", "공부하기"],
+    doing: [],
+    done: [],
+  },
 });

2a1d710f75901c3bdd88d3e642602de63f2ce995
tjdrkr2580 <tjdrkr2580@naver.com>
2023-02-07 05:31:16 +0900

    ADD | Styling

--- trello/src/Card.tsx
+++ trello/src/Card.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import { Draggable } from "react-beautiful-dnd";
 import styled from "styled-components";
 
-const CardWrapper = styled.div`
+const CardWrapper = styled.div<{ isDragging: boolean }>`
   width: 90%;
   border-radius: 0.25rem;
-  background-color: #e8e8e7;
+  background-color: ${(props) => (!props.isDragging ? "#ececec" : "#b6b5b5")};
+  transition: 0.25s ease-in-out background-color;
+  box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
   padding: 0.5rem 0.5rem;
 `;
 
@@ -17,8 +19,9 @@ interface propsType {
 const Card = ({ todo, index }: propsType) => {
   return (
     <Draggable key={todo} draggableId={todo} index={index}>
-      {(magic) => (
+      {(magic, snapshot) => (
         <CardWrapper
+          isDragging={snapshot.isDragging}
           ref={magic.innerRef}
           {...magic.draggableProps}
           {...magic.dragHandleProps}
